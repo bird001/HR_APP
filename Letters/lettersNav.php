f37f3f6454c8c7a91913ab15e5f4f165faee8261
bird001 <bird.samoht@gmail.com>
2017-04-25 13:18:19 -0500

    minor change

--- Letters/lettersNav.php
+++ Letters/lettersNav.php
@@ -6,14 +6,14 @@ include("../db/db3.php");
 $email = $_SESSION['login_user'];
 $result = mysqli_query($conn, "select * from Users where EmpEmail = '$email' ");
 $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-$role = $row['EmpRole'];
+$dept = $row['EmpDept'];
 ?>
 
 <nav id="primary-navigation" class="row">
     <div class="col-lg-12">
         <ul class="nav nav-tabs">
             <?php
-            if ($role == 'Manager') {
+            if ($dept == 'HR') {
                 ?>
             <li><a  href="disciplinaryletters">Upload Disciplinary</a></li>
             <li><a  href="lettertemplates">Letter Templates</a></li>
